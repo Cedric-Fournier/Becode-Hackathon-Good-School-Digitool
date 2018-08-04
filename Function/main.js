158bbe507d76be866fb9f1a2f86e31de77e0ca61
Adrien d'Oreye <adrien.doreye@gmail.com>
2018-08-05 01:12:18 +0200

    changement cdn jquery min--> total

--- Function/main.js
+++ Function/main.js
@@ -1,48 +1,31 @@
-// let modalsAction = () => {
-//
-//   $("#mySignIn").click(function() {
-//     $("#myModalSignIn").modal();
-//   });
-//
-//   $("#mySignInRegister").click(function() {
-//     // $("#myModalSignIn").hide() && $("#myModalSignUp").modal();
-//     $("#myModalSignIn").modal("hide") && $("#myModalSignUp").modal();
-//   });
-//
-//   $("#mySignUp").click(function() {
-//     $("#myModalSignUp").modal();
-//   });
-//
-// };
-
 //integration json en modal bs
+  //modal Eau
+  $(document).on("click", "#clicktypeEau", function() {
+    $.getJSON("../Assets/JSON/action.json", function(data) {
+      console.log('test');
+      let rand = Math.floor(Math.random() * 3) + 1;
+      console.log(rand);
+      var action = data[1].action[rand];
+      $('.actionTitle').html('Eau');
+      $('.doAction').html('<p> ' + action + ' </p>');
+    });
+  });
 
-//modal Eau
-$(document).on("click", "#clicktypeEau", function() {
-  $.getJSON("./Assets/JSON/action.json", function(data) {
-    let rand = Math.floor(Math.random() * 3) + 1;
-    console.log(rand);
-    var action = data[1].action[rand];
-    $('.actionTitle').html('Eau');
-    $('.doAction').html('<p> ' + action + ' </p>');
+  //modal energie
+  $(document).on("click", "#clicktypeEnergie", function() {
+    $.getJSON("../Assets/JSON/action.json", function(data) {
+      let rand = Math.floor(Math.random() * 3) + 1;
+      var action = data[0].action[rand];
+      $('.actionTitle').html('Energie');
+      $('.doAction').html('<p> ' + action + ' </p>');
+    });
   });
-});
-
-//modal energie
-$(document).on("click", "#clicktypeEnergie", function() {
-  $.getJSON("./Assets/JSON/action.json", function(data) {
-    let rand = Math.floor(Math.random() * 3) + 1;
-    var action = data[0].action[rand];
-    $('.actionTitle').html('Energie');
-    $('.doAction').html('<p> ' + action + ' </p>');
+  //modal dechets
+  $(document).on("click", "#clicktypeDechet", function() {
+    $.getJSON("../Assets/JSON/action.json", function(data) {
+      let rand = Math.floor(Math.random() * 3) + 1;
+      var action = data[2].action[rand];
+      $('.actionTitle').html('Déchets');
+      $('.doAction').html('<p>' + action + ' </p>');
+    });
   });
-});
-//modal dechets
-$(document).on("click", "#clicktypeDechet", function() {
-  $.getJSON("./Assets/JSON/action.json", function(data) {
-    let rand = Math.floor(Math.random() * 3) + 1;
-    var action = data[2].action[rand];
-    $('.actionTitle').html('Déchets');
-    $('.doAction').html('<p>' + action + ' </p>');
-  });
-});
