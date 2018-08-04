a6d952cd98bbebabbd5f078d302304c5e4e03562
Adrien d'Oreye <adrien.doreye@gmail.com>
2018-08-04 22:11:18 +0200

    correction titre modal

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -6,7 +6,7 @@
         let rand = Math.floor(Math.random() * 3) + 1;
         console.log(rand);
         var action = data[1].action[rand];
-          $('.actionTitle').html('Energie');
+          $('.actionTitle').html('Eau');
           $('.doAction').html('<p> '+ action+' </p>');
         }
       );
@@ -17,7 +17,7 @@
     $.getJSON( "./assets/js/action.json", function(data) {
         let rand = Math.floor(Math.random() * 3) + 1;
         var action = data[0].action[rand];
-          $('.actionTitle').html('Eau');
+          $('.actionTitle').html('Energie');
           $('.doAction').html('<p> '+ action+' </p>');
         }
       );
