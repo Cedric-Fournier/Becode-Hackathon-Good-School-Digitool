2b80c4221941b6825ce5a241e543814c61efcc90
Adrien d'Oreye <adrien.doreye@gmail.com>
2018-08-04 19:30:15 +0200

    correction inversion eau energie

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,20 +1,10 @@
 //integration json en modal bs
 
-
+  //modal Eau
   $(document).on("click", "#clicktypeEau", function (){
     $.getJSON( "./assets/js/action.json", function(data) {
         let rand = Math.floor(Math.random() * 3) + 1;
         console.log(rand);
-        var action = data[0].action[rand];
-          $('.actionTitle').html('Eau');
-          $('.doAction').html('<p> '+ action+' </p>');
-        }
-      );
-  });
-
-  $(document).on("click", "#clicktypeEnergie", function (){
-    $.getJSON( "./assets/js/action.json", function(data) {
-        let rand = Math.floor(Math.random() * 3) + 1;
         var action = data[1].action[rand];
           $('.actionTitle').html('Energie');
           $('.doAction').html('<p> '+ action+' </p>');
@@ -22,6 +12,17 @@
       );
   });
 
+  //modal energie
+  $(document).on("click", "#clicktypeEnergie", function (){
+    $.getJSON( "./assets/js/action.json", function(data) {
+        let rand = Math.floor(Math.random() * 3) + 1;
+        var action = data[0].action[rand];
+          $('.actionTitle').html('Eau');
+          $('.doAction').html('<p> '+ action+' </p>');
+        }
+      );
+  });
+  //modal dechets
   $(document).on("click", "#clicktypeDechet", function (){
     $.getJSON( "./assets/js/action.json", function(data) {
         let rand = Math.floor(Math.random() * 3) + 1;
